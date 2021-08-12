0466f7e16c063aa49bf23b5acf0668ce7b08c8fd
marujore <rennanmarujo@gmail.com>
2021-08-12 14:34:18 -0300

    Sen2cor Docker

---
+++ 2.5.5/run_sen2cor.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+shopt -s nullglob
+if [ $1 == "--help" ]; then
+    echo "Usage: \
+    docker run --rm \
+    -v /path/to/input/:/mnt/input-dir \
+    -v /path/to/output:/mnt/output-dir \
+    -v /path/to/CCI4SEN2COR:/home/lib/python2.7/site-packages/sen2cor/aux_data \
+    -v /path/to/sen2cor/2.5/cfg:/root/sen2cor/2.5/cfg \
+    -t sen2cor:2.5.5 <SENTINEL-2.SAFE>"
+    exit 0
+fi
+
+# Set default directories to the INDIR and OUTDIR
+# You can customize it using INDIR=/my/custom OUTDIR=/my/out run_sen2cor_fmask.sh
+if [ -z "${INDIR}" ]; then
+    INDIR=/mnt/input-dir
+fi
+
+if [ -z "${OUTDIR}" ]; then
+    OUTDIR=/mnt/output-dir/
+fi
+
+## SENTINEL-2
+SAFENAME_L1C=$1
+SAFENAME_L2A=${SAFENAME_L1C//L1C/L2A}
+SAFENAME_L2A=${SAFENAME_L2A::45}
+SAFEDIR_L1C=${INDIR}/${SAFENAME_L1C}
+
+WORKDIR=/work
+# Ensure that workdir/sceneid is clean
+if [ -d "${WORKDIR}/${SAFENAME_L1C}" ]; then
+    rm -r ${WORKDIR}/${SAFENAME_L1C}
+fi
+cp -r ${SAFEDIR_L1C} ${WORKDIR}
+
+# Process Sen2cor
+cd ${WORKDIR}
+/home/bin/L2A_Process ${SAFENAME_L1C}
+
+for entry in `ls ${WORKDIR}`; do
+    if [[ $entry == "$SAFENAME_L2A"* ]]; then
+        SAFENAME_L2A=$entry
+    fi
+done
+
+cp -r ${WORKDIR}/${SAFENAME_L2A} $OUTDIR
+rm -r $WORKDIR/${SAFENAME_L2A}
+rm -r $WORKDIR/${SAFENAME_L1C}
+exit 0
